428e848feb5ee2947e9ccdb39cc43a6af256b29e
Ole Martin Ruud <barskern@outlook.com>
2019-12-16 20:30:48 +0100

    Use duckduckgo.com as homepage

--- config.def.h
+++ config.def.h
@@ -1,3 +1,5 @@
+#define HOMEPAGE "https://duckduckgo.com/"
+
 /* modifier 0 means no modifier */
 static int surfuseragent    = 1;  /* Append Surf version to default WebKit user agent */
 static char *fulluseragent  = ""; /* Or override the whole user agent string */
