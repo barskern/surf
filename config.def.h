b05f6817ad8486ff565b864ed315a5a002f232ec
Ole Martin Ruud <barskern@outlook.com>
2019-12-20 22:08:34 +0100

    Set cookie policy to no thirdparty cookies

--- config.def.h
+++ config.def.h
@@ -26,7 +26,7 @@ static Parameter defconfig[ParameterLast] = {
 	[AccessWebcam]        =       { { .i = 0 },     },
 	[Certificate]         =       { { .i = 0 },     },
 	[CaretBrowsing]       =       { { .i = 0 },     },
-	[CookiePolicies]      =       { { .v = "@Aa" }, },
+	[CookiePolicies]      =       { { .v = "@" }, },
 	[DefaultCharset]      =       { { .v = "UTF-8" }, },
 	[DiskCache]           =       { { .i = 1 },     },
 	[DNSPrefetch]         =       { { .i = 0 },     },
